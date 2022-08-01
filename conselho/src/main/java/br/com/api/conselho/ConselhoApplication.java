f7f6370a583b29ea28f64e494080544a05f6a50a
andersonmedeiros <andersonandrade.30@gmail.com>
2022-07-31 21:00:19 -0400

    criando projeto

---
+++ conselho/src/main/java/br/com/api/conselho/ConselhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.conselho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConselhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConselhoApplication.class, args);
+	}
+
+}
---
+++ conselho/src/test/java/br/com/api/conselho/ConselhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.conselho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConselhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
